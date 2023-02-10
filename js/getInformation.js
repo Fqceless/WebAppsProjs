2c42963c89c5045f01b4d2d60ae278c7bd484a33
Vix <eatahorsequickly@gmail.com>
2023-02-09 21:11:41 -0500

    regs
    r

--- js/getInformation.js
+++ js/getInformation.js
@@ -1,48 +1,46 @@
 let myform = document.getElementById("form");
 
-myform.addEventListener("keydown", function (event) {
-  event.preventDefault();
-  
-  let ligmaObject = {
-    "fname":document.getElementById("fname").value,
-    "email":document.getElementById("email").value,
-    "startYear":document.getElementById("startYear").value,
-    "lname":document.getElementById("lname").value,
-  }
-
-  validateFields(ligmaObject);
-});
 
 myform.addEventListener("submit", function (event) {
   event.preventDefault();
 
-
-  let ligmaObject = {
-    "fname":document.getElementById("fname").value,
-    "email":document.getElementById("email").value,
-    "startYear":document.getElementById("startYear").value,
-    "lname":document.getElementById("lname").value,
+  
+  if (ValidateForm == 1) {
+    alert("Please fill out all fields");
+    return;
   }
 
-  if (validateFields(ligmaObject)){
-    post('/all/', ligmaObject);
+  
+  else if (ValidateForm == 2) {    
+    alert("Please enter a valid email");
+    return;
   }
+  
+
+  post('',{fname,email,startYear,lname})
+});
+myform.addEventListener("keyup", function (event) {
+  event.preventDefault();
+    
+  ValidateForm();
+    
 });
 
-function validateFields(obj){
+function ValidateForm(){
+  fname = document.getElementById("fname").value;
+    email = document.getElementById("email").value;
+    startYear = document.getElementById("startYear").value;
+    lname = document.getElementById("lname").value;
 
-  // Validate form filling
-  //if (!obj.fname || !obj.email || !obj.startYear || !obj.lname) {
-  //  alert("Please fill out all fields");
-  //  return;
-  //}
-
-  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!obj.email.match(emailRegex)) {
-    alert("Please enter a valid email");
-    return false;
+  //Validate form filling
+  if (!fname || !email || !startYear || !lname) {
+    return 1;
   }
 
-  return true;
-
+  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email.match(emailRegex)) {    
+    return 2;
+  }
+  //good
+  return 0;
 }
