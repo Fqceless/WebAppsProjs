da0f95825dd12e4ef1e02bd08add2f32b9e5bb35
Vix <eatahorsequickly@gmail.com>
2023-02-09 19:49:57 -0500

    post
    hahahah

--- js/basicFunctions.js
+++ js/basicFunctions.js
@@ -25,3 +25,24 @@ function clickCounter() {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
   }
 }
+
+function post(path, params, method='post') {
+
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = "http://judah.cedarville.edu/echo.php";
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
--- js/getInformation.js
+++ js/getInformation.js
@@ -1,45 +1,48 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", function (event) {
+let myform = document.getElementById("form");
+
+myform.addEventListener("keydown", function (event) {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const startYear = document.getElementById("startYear").value;
-  const lname = document.getElementById("lname").value;
-
-  // Validate form on submit
-  if (!fname || !email || !startYear || !lname) {
-    alert("Please fill out all fields");
-    return;
+  
+  let ligmaObject = {
+    "fname":document.getElementById("fname").value,
+    "email":document.getElementById("email").value,
+    "startYear":document.getElementById("startYear").value,
+    "lname":document.getElementById("lname").value,
   }
 
-  // Validate email using regex
-  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!email.match(emailRegex)) {
-    alert("Please enter a valid email");
-    return;
-  }
-
-  // Validate name using regex
-  const fnameRegex = /^[a-z ]+$/i;
-  if (!fname.match(fnameRegex)) {
-    alert("Please enter a valid name");
-    return;
-  }
-
-  // Validate name using regex
-  const startYearRegex = /^"20"+[1-9]{2}+$/;
-  if (!startYear.match(startYearRegex)) {
-    alert("Please enter a valid year");
-    return;
-  }
-
-  // Validate name using regex
-  const lnameRegex = /^[a-z ]+$/i;
-  if (!lname.match(lnameRegex)) {
-    alert("Please enter a valid name");
-    return;
-  }
-
-  // Submit form data to http://judah.cedarville.edu/echo.php
-  // ...
+  validateFields(ligmaObject);
 });
+
+myform.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+
+  let ligmaObject = {
+    "fname":document.getElementById("fname").value,
+    "email":document.getElementById("email").value,
+    "startYear":document.getElementById("startYear").value,
+    "lname":document.getElementById("lname").value,
+  }
+
+  if (validateFields(ligmaObject)){
+    post('/all/', ligmaObject);
+  }
+});
+
+function validateFields(obj){
+
+  // Validate form filling
+  //if (!obj.fname || !obj.email || !obj.startYear || !obj.lname) {
+  //  alert("Please fill out all fields");
+  //  return;
+  //}
+
+  let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!obj.email.match(emailRegex)) {
+    alert("Please enter a valid email");
+    return false;
+  }
+
+  return true;
+
+}
