fab60c7c5c2c0707a87118b69ca65600d3f64a8a
Christopher LaFave <clafave@cedarville.edu>
2023-02-13 21:53:28 -0500

    objects and monthboxes work!

--- js/objects.js
+++ js/objects.js
@@ -21,12 +21,21 @@ class course{
     }
 }
 
+class semester{
+    constructor(term){
+        this.term = term;
+        this.courses = [];
+    }
+}
+
 class year{
     constructor(yearNum){
         this.yearNum = yearNum;
-        this.springArray = [];
-        this.summerArray = [];
-        this.fallArray = [];
+        this.semesters = [
+            new semester('Spring'),
+            new semester('Summer'),
+            new semester('Fall')
+        ];
     }
 }
 
@@ -50,50 +59,96 @@ let webApps = new course(
 
 myPlan.courseArray.push(webApps);
 
-let year2023 = new year(2023);
-year2023.springArray.push(myPlan.courseArray[0]);
 
 console.log(myPlan);
 console.log(webApps);
-console.log(year2023);
 
 
-let semesters = [
-    {
-        yearNum: 2021,
-        fallArray: [
+let yearsArray = [];
 
-        ]
-    },
-    {
-      term: 'Spring',
-      year: 2022,
-      courses: []
-    },
-    {
-      term: 'Fall',
-      year: 2023,
-      courses: []
-    }
-  ];
+yearsArray[0] = new year(2021);
+yearsArray[0].semesters[2].courses.push(new course(
+    'Fall',
+    '2021',
+    'CS-1210',
+    'C++ Programming',
+    '2'
+));
 
-  for (let semester of semesters) {
-    let monthbox = `
-    <div class="Monthbox" id="${semester.term}${semester.year}">
-    <h1>${semester.term} ${semester.year}</h1>
-    <ul>
-    `;
-    
-    for (let course of semester.courses) {
-    monthbox += `
-    <li>${course.courseNum} ${course.courseName} ${course.courseCredits}</li>
-    `;
-    }
-    
-    monthbox += `
-    </ul>
-    </div>
-    `;
-    
-    document.write(monthbox);
-    }
+yearsArray[1] = new year(2022);
+yearsArray[1].semesters[0].courses.push(new course(
+    'Spring',
+    '2022',
+    'CS-1220',
+    'Object-Oriented Design in C++',
+    '3'
+));
+yearsArray[1].semesters[1].courses.push(new course(
+    'Summer',
+    '2022',
+    'HUM-1400',
+    'Introduction to the Humanities',
+    '3'
+));
+yearsArray[1].semesters[2].courses.push(new course(
+    'Fall',
+    '2022',
+    'CS-2210',
+    'Data Structures Using Java',
+    '3'
+));
+
+
+yearsArray[2] = new year(2023);
+yearsArray[2].semesters[0].courses.push(new course(
+    'Spring',
+    '2023',
+    'CS-3220',
+    'Web Applications',
+    '3'
+));
+yearsArray[2].semesters[1].courses.push(new course(
+    'Summer',
+    '2023',
+    'GSS-1100',
+    'Politics and American Culture',
+    '3'
+));
+yearsArray[2].semesters[2].courses.push(new course(
+    'Fall',
+    '2023',
+    'CS-3410',
+    'Algorithms',
+    '3'
+));
+
+yearsArray[3] = new year(2024);
+yearsArray[3].semesters[0].courses.push(new course(
+    'Spring',
+    '2024',
+    'CS-3510',
+    'Compiler Theory and Practice',
+    '3'
+));
+yearsArray[3].semesters[1].courses.push(new course(
+    'Summer',
+    '2024',
+    'HIST-1110',
+    'United States History I',
+    '3'
+));
+yearsArray[3].semesters[2].courses.push(new course(
+    'Fall',
+    '2024',
+    'CS-4810',
+    'Software Engineering I',
+    '3'
+));
+yearsArray[4] = new year(2025);
+yearsArray[4].semesters[0].courses.push(new course(
+    'Spring',
+    '2025',
+    'CS-4820',
+    'Software Engineering II',
+    '4'
+));
